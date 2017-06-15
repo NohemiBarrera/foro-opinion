69f25ddb6bbb8282cc8446acf6ee2e91d7dbab20
Nohemi Barrera <nohemi.baol@gmail.com>
2017-06-15 16:55:03 -0500

    comentarios

--- js/main.js
+++ js/main.js
@@ -2,20 +2,26 @@ var api = {
   url: 'http://examen-laboratoria-sprint-5.herokuapp.com/topics'
 };
 
+
+
 var $themesList = $("#themes-list");
 
 var cargarPagina = function () {
 	cargarTemas();
 	$("#add-form").submit(agregarTema);
+	$(document).on("click", ".detalle", mostrarDetalle);
+	$("#search-form").submit(filtrarTemas);
+	
 };
 
 var cargarTemas = function (){
 	$.getJSON(api.url, function (temas){
 		temas.forEach(crearTema);
+		temas.forEach(filtrarTemas);
 	});
 };
 
-var crearTema = function (tema){
+function crearTema (tema){
 	var contenido = tema.content;
 	var nombreAutor = tema.author_name;
 	var respuestas = tema.responses_count;
@@ -23,6 +29,7 @@ var crearTema = function (tema){
 	var $tr = $("<tr />");
 	//celda del tema
 	var $nombreTema = $("<td />");
+	$nombreTema.addClass("detalle")
 	$nombreTema.text(contenido);
 	//celda del autor
 	var $nombreAutor = $("<td />");
@@ -39,7 +46,7 @@ var crearTema = function (tema){
 
 };
 
-var agregarTema = function(e){
+function agregarTema (e){
 	e.preventDefault();
 	var nombreTema = $("#tema").val();
 	var nombreAutor = $("#autor").val();
@@ -53,6 +60,31 @@ var agregarTema = function(e){
 		$("#myModal").modal("hide");
 	});
 };
+var mostrarDetalle = function(){
+
+};
+
+var filtrarTemas = function (){
+ /* La idea es filtrar el tema, para esto sería necesario recuperar content directamente de la api 
+ 	y comparar el valor del input para que así sólo muestre aquellos que coincidan, creo que en este 
+ 	caso habría sido mejor haber utilizado una plantilla para así poder cambiar los valores más rápido
+ 	pero ya no puedo pensar
+ 	 */
+};
+
+/*var filtrarTemas = function(e){
+	//e.preventDefault();
+//	var contenido = api.url;
+
+//En este punto ya no sé qué estoy haciendo, aiuda :( 
+
+	var criterio = $("#buscar").val().toLowerCase();
+	var resultados = api.url.filter(function(tema){
+		return tema.toLowerCase().indexOf(criterio)>=0;
+	});
+	console.log(contenido);
+};
+*/
 
 
 
